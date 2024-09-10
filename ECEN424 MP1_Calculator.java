4d67fc102bc5ae46d18d2013335cef33b91cda1a
falzapiain <145292708+falzapiain@users.noreply.github.com>
2024-09-10 11:15:05 -0500

    Add files via upload

---
+++ ECEN424 MP1_Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    private String name;
+
+    public Float addition(Float A, Float B)
+    public Float subtraction(Float A, FLoat B)
+    public Float multiplication(Float A, Float B)
+    public void setname(String N)
+    public String getname()
+
+}
