47156a51196157086cae4e5476e7228dfca0dd4f
falzapiain <145292708+falzapiain@users.noreply.github.com>
2024-09-11 16:24:47 -0500

    Update ECEN424 MP1_Calculator.java

--- ECEN424 MP1_Calculator.java
+++ ECEN424 MP1_Calculator.java
@@ -2,9 +2,25 @@ public class Calculator {
     private String name;
 
     public Float addition(Float A, Float B)
+    {
+        return A + B;
+    }
     public Float subtraction(Float A, FLoat B)
+    {
+        return A - B;
+    }
     public Float multiplication(Float A, Float B)
+    {
+        return A*B;
+    }
     public void setname(String N)
+    {
+        name = N;
+    }
+
     public String getname()
+    {
+        return name;
+    }
 
 }
